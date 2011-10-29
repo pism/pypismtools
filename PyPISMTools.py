eaf9ce1924a4a84023848589ed073aae8efbaa6a
Andy Aschwanden <andy.aschwanden@arsc.edu>
2011-10-29 09:21:00 +0200

    - added routine to import GMT colormaps and use it with pylab.

--- PyPISMTools.py
+++ PyPISMTools.py
@@ -9,11 +9,13 @@ determines information about an experiment from the netcdf file
 directly, especially from the 'pism_overrides' flag. Such information
 can then be used for labeling, plotting, evaluation, etc. The indend
 is to provide a robust tool to evaluate data, and to avoid common mistakes
-such as mis-labeling plots.
+such as mis-labeling plots. Additional functions include routines to
+permute (netcdf) dimension, convert units using udunits, and to import
+GMT colormaps.
 '''
 __author__ = "Andy Aschwanden"
 
-__all__ = ['norm_infinity','norm_Lp','norm_2','norm_1','get_rmse','get_avg','unit_converter','permute','plot_mapview','plot_histogram','plot_histogram2','print_info','print_overall_statistics','Observation','Experiment']
+__all__ = ['gmtColormap','norm_infinity','norm_Lp','norm_2','norm_1','get_rmse','get_avg','unit_converter','permute','plot_mapview','plot_histogram','plot_histogram2','print_info','print_overall_statistics','Observation','Experiment']
 
 import numpy as np
 import pylab as plt
@@ -25,6 +27,94 @@ except:
 
 ## FIXME: how to provide DEBUG flag to module
 DEBUG=None
+
+def gmtColormap(fileName):
+    '''
+    Import a CPT colormap from GMT.
+    
+    Parameters
+    ----------
+    fileName : a cpt file.
+    
+    Example
+    -------
+    >>> cdict = gmtColormap('mycolormap.cpt')
+    >>> gmt_colormap = colors.LinearSegmentedColormap('my_colormap',cdict)
+
+    Notes
+    -----
+    This code snipplet is from http://www.mail-archive.com/matplotlib-users@lists.sourceforge.net/msg09547.html
+    '''
+    import colorsys
+      
+    try:
+        f = open(fileName)
+    except:
+        print "file ",fileName, "not found"
+        return None
+    
+    lines = f.readlines()
+    f.close()
+    
+    x = []
+    r = []
+    g = []
+    b = []
+    colorModel = "RGB"
+    for l in lines:
+        ls = l.split()
+        if l[0] == "#":
+            if ls[-1] == "HSV":
+                colorModel = "HSV"
+                continue
+            else:
+                continue
+        if ls[0] == "B" or ls[0] == "F" or ls[0] == "N":
+            pass
+        else:
+            x.append(float(ls[0]))
+            r.append(float(ls[1]))
+            g.append(float(ls[2]))
+            b.append(float(ls[3]))
+            xtemp = float(ls[4])
+            rtemp = float(ls[5])
+            gtemp = float(ls[6])
+            btemp = float(ls[7])
+
+    x.append(xtemp)
+    r.append(rtemp)
+    g.append(gtemp)
+    b.append(btemp)
+
+    nTable = len(r)
+    x = np.array( x , np.float32)
+    r = np.array( r , np.float32)
+    g = np.array( g , np.float32)
+    b = np.array( b , np.float32)
+    if colorModel == "HSV":
+        for i in range(r.shape[0]):
+            rr,gg,bb = colorsys.hsv_to_rgb(r[i]/360.,g[i],b[i])
+            r[i] = rr ; g[i] = gg ; b[i] = bb
+    if colorModel == "HSV":
+        for i in range(r.shape[0]):
+            rr,gg,bb = colorsys.hsv_to_rgb(r[i]/360.,g[i],b[i])
+            r[i] = rr ; g[i] = gg ; b[i] = bb
+    if colorModel == "RGB":
+        r = r/255.
+        g = g/255.
+        b = b/255.
+    xNorm = (x - x[0])/(x[-1] - x[0])
+
+    red = []
+    blue = []
+    green = []
+    for i in range(len(x)):
+        red.append([xNorm[i],r[i],r[i]])
+        green.append([xNorm[i],g[i],g[i]])
+        blue.append([xNorm[i],b[i],b[i]])
+    colorDict = {"red":red, "green":green, "blue":blue}
+    return (colorDict)
+
 
 def norm_infinity(var):
     '''
