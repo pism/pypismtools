5c4c247575f1be96fd8d0d6eeb3b78ed2a3058f6
Andy Aschwanden <andy.aschwanden@arsc.edu>
2012-04-25 17:06:12 -0800

    - added simple dependency checking.

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
 from distutils.core import setup
 
 PKG_NAME = 'PyPISMTools'
+print('\nInstalling %s' % PKG_NAME)
+print('----------------------------')
+
+packages = ('numpy', 'osgeo', 'netCDF4', 'udunits2', 'pyproj',
+            'mpl_toolkits.basemap', 'mpl_toolkits.axes_grid1')
+print('\nChecking dependencies:')
+not_installed = []
+for package in packages:
+    try:
+        __import__(package)
+        print('  - % s package is installed' % package)
+    except ImportError:
+        print('  - % s package not installed' % package)
+        not_installed.append(package)
+if not_installed:
+    print('Please install the following packages:')
+    for package in not_installed:
+        print('  - %s' % package)
+    print('Exiting')
+    import sys
+    sys.exit()
+
 
 setup(name=PKG_NAME,
       version='0.12',
