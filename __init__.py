dc0ac5894ef02e0b4cf18da5d0626cc0c218daec
Andy Aschwanden <aaschwanden@alaska.edu>
2013-11-28 15:30:30 +0100

    moved create_profile_axis back into extract-profile.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-__all__ = ['create_profile_axis', 'read_textfile', 'read_shapefile', 'GeoTIFF',
-           'get_dims', 'get_projection_from_file', 'add_inner_title',
-           'get_golden_mean', 'set_mode', 'trend_estimator',
-           'colorList', 'gmtColormap', 'smooth', 'fftsmooth', 'get_rmse', 'get_avg',
-           'unit_converter', 'permute', 'plot_mapview', 'plot_histogram',
-           'plot_histogram2', 'print_info', 'print_overall_statistics',
-           'Observation', 'Experiment']
+__all__ = ['GeoTIFF', 'get_dims', 'get_projection_from_file', 
+           'add_inner_title', 'get_golden_mean', 'set_mode',
+           'trend_estimator', 'colorList', 'gmtColormap', 
+           'smooth', 'fftsmooth', 'get_rmse', 'get_avg', 
+           'unit_converter', 'permute', 'plot_mapview', 
+           'plot_histogram', 'plot_histogram2', 'print_info', 
+           'print_overall_statistics', 'Observation', 'Experiment']
 
