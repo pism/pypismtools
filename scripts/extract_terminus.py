a6868ff0111a65874d47505ff06987c93668a697
Andy Aschwanden <aaschwanden@alaska.edu>
2015-08-25 16:06:55 -0800

    further steps. Can convert polygon to polyline, then select ocean and floating portions. Now on to figuring out how to buffer a line.

--- scripts/extract_terminus.py
+++ scripts/extract_terminus.py
@@ -39,41 +39,6 @@ class ShapeDataError(Exception):
     pass
 
 
-def get_contours(array, x, y, projection, level):
-    '''
-    Find contours for a given level
-    '''
-
-    # Find contours at a constant value
-    contours = sorted(measure.find_contours(array, level),
-                      key=lambda x: len(x))
-
-    i = range(0, len(x))
-    j = range(0, len(y))
-
-    lon = []
-    lat = []
-    contour_points = []
-    for k in range(0, len(contours)):
-        contour = contours[k]
-        contour_x = x[0] + contour[:,1] * (x[-1]-x[0])/(len(i)-1)
-        contour_y = y[0] + contour[:,0] * (y[-1]-y[0])/(len(j)-1)
-        # Convert to EPSG:4326
-        contour_lon, contour_lat = projection(contour_x, contour_y, inverse=True)
-        lon.append(contour_lon)
-        lat.append(contour_lat)
-        points = [(contour_lon[k], contour_lat[k]) for k in range(len(contour_lat))]
-        contour_points.append(points)
-    # reverse direction, last entry (longest contour) first.
-    contour_points.reverse()
-
-    if single:
-        contour_points = [contour_points[0]]
-
-    return contour_points
-
-
-
 parser = ArgumentParser(description='''A script to extract a (closed) contour line from a variable in a netCDF file, and save it as a shapefile (polygon).''')
 parser.add_argument("FILE", nargs=1)
 parser.add_argument("-o", "--output_filename", dest="out_file",
@@ -136,48 +101,50 @@ if poly_layer is None:
     srs = None
     if src_ds.GetProjectionRef() != '':
         srs = osr.SpatialReference()
-        srs.ImportFromWkt( src_ds.GetProjection())
-        
+        srs.ImportFromWkt(src_ds.GetProjection())
+
     poly_layer = mem_ds.CreateLayer('poly', srs, ogr.wkbPolygon)
 
     if dst_fieldname is None:
         dst_fieldname = 'DN'
         
-    fd = ogr.FieldDefn( dst_fieldname, ogr.OFTInteger )
-    poly_layer.CreateField( fd )
-    dst_field = 0
+    fd = ogr.FieldDefn(dst_fieldname, ogr.OFTInteger)
+    poly_layer.CreateField(fd)
+    poly_dst_field = 0
 else:
     if dst_fieldname is not None:
-        dst_field = poly_layer.GetLayerDefn().GetFieldIndex(dst_fieldname)
-        if dst_field < 0:
+        poly_dst_field = poly_layer.GetLayerDefn().GetFieldIndex(dst_fieldname)
+        if poly_dst_field < 0:
             print("Warning: cannot find field '%s' in layer '%s'" % (dst_fieldname, dst_layername))
 
-# try:
-#     line_layer = mem_ds.GetLayerByName(dst_layername)
-# except:
-#     line_layer = None
+try:
+    line_layer = mem_ds.GetLayerByName(dst_layername)
+except:
+    line_layer = None
 
-# if line_layer is None:
+if line_layer is None:
 
-#     srs = None
-#     if src_ds.GetProjectionRef() != '':
-#         srs = osr.SpatialReference()
-#         srs.ImportFromWkt( src_ds.GetProjectionRef)
+    srs = None
+    if src_ds.GetProjectionRef() != '':
+        srs = osr.SpatialReference()
+        srs.ImportFromWkt(src_ds.GetProjectionRef())
         
-#     line_layer = mem_ds.CreateLayer('line', srs, ogr.wkbMultiLineString)
+    line_layer = mem_ds.CreateLayer('line', srs, ogr.wkbMultiLineString)
 
-#     if dst_fieldname is None:
-#         dst_fieldname = 'DN'
+    if dst_fieldname is None:
+        dst_fieldname = 'DN'
         
-#     fd = ogr.FieldDefn( dst_fieldname, ogr.OFTInteger )
-#     line_layer.CreateField( fd )
-#     dst_field = 0
-# else:
-#     if dst_fieldname is not None:
-#         dst_field = line_layer.GetLayerDefn().GetFieldIndex(dst_fieldname)
-#         if dst_field < 0:
-#             print("Warning: cannot find field '%s' in layer '%s'" % (dst_fieldname, dst_layername))
+    fd = ogr.FieldDefn(dst_fieldname, ogr.OFTInteger)
+    line_layer.CreateField(fd)
+    line_dst_field = 0
+else:
+    if dst_fieldname is not None:
+        line_dst_field = line_layer.GetLayerDefn().GetFieldIndex(dst_fieldname)
+        if line_dst_field < 0:
+            print("Warning: cannot find field '%s' in layer '%s'" % (dst_fieldname, dst_layername))
 
+ocean_value = 4
+floating_value = 3
             
 # for k, t in enumerate(time):
 for k in range(src_ds.RasterCount):
@@ -185,26 +152,42 @@ for k in range(src_ds.RasterCount):
         #timestamp = cdftime.num2date(t)
         #print('Processing {}'.format(timestamp))
         srcband = src_ds.GetRasterBand(k+1)
-        result = gdal.Polygonize(srcband, None, poly_layer, dst_field, [],
+        result = gdal.Polygonize(srcband, None, poly_layer, poly_dst_field, [],
                           callback = gdal.TermProgress)
-        poly_layer.SetAttributeFilter("{} = 0".format(dst_fieldname))
-        # geomcol =  ogr.Geometry(ogr.wkbGeometryCollection)
-        # for feature in poly_layer:
+        poly_layer.SetAttributeFilter("{} = {}".format(dst_fieldname, ocean_value))
+        for m, feature in enumerate(poly_layer):
+            geom = feature.GetGeometryRef()
+            ring = geom.GetGeometryRef(0)
+            # print ring.GetPointCount()
+            print feature.GetField(dst_fieldname)
+            # Create feature
+            featureDefn = line_layer.GetLayerDefn()
+            line_feature = ogr.Feature(featureDefn)
+            line_feature.SetGeometry(ring)
+            line_feature.SetFID(m)
+            i = line_feature.GetFieldIndex(dst_fieldname)
+            line_feature.SetField(i, feature.GetField(dst_fieldname))
+            line_layer.CreateFeature(line_feature)
+        poly_layer.SetAttributeFilter("{} = {}".format(dst_fieldname, floating_value))
+        # for m, feature in enumerate(poly_layer):
         #     geom = feature.GetGeometryRef()
         #     ring = geom.GetGeometryRef(0)
-        #     geomcol.AddGeometry(ring)
-        # geomcol =  ogr.Geometry(ogr.wkbGeometryCollection)
-        # featureDefn = line_layer.GetLayerDefn()
-        # line_feature = ogr.Feature(featureDefn)
-        # line_feature.SetGeometry(geomcol)
-        # line_layer.CreateFeature(line_feature)
+        #     # print ring.GetPointCount()
+        #     print feature.GetField(dst_fieldname)
+        #     # Create feature
+        #     featureDefn = line_layer.GetLayerDefn()
+        #     line_feature = ogr.Feature(featureDefn)
+        #     line_feature.SetGeometry(ring)
+        #     line_feature.SetFID(m)
+        #     i = line_feature.GetFieldIndex(dst_fieldname)
+        #     line_feature.SetField(i, feature.GetField(dst_fieldname))
+        #     line_layer.CreateFeature(line_feature)
         
         
-poly_layer = None
-#line_layer = None
-mem_ds = None
+# poly_layer = None
+# line_layer = None
+# mem_ds = None
 
-# save(shp_filename, contour_points, level)
 
 
 nc.close()
