07397ae3cd2eb57f0cb7626b7d08b94864ba623b
Andy Aschwanden <aaschwanden@alaska.edu>
2016-02-29 17:38:14 -0900

    added script to generate extended Greenland grid, cell corners included

--- scripts/create_greenland_ext_epsg3413_grid.py
+++ scripts/create_greenland_ext_epsg3413_grid.py
@@ -23,7 +23,7 @@ grid_spacing = options.grid_spacing  # convert
 fileformat = options.fileformat.upper()
 
 if len(args) == 0:
-    nc_outfile = 'grn' + str(grid_spacing) + 'm.nc'
+    nc_outfile = 'grn' + str(int(grid_spacing)) + 'm.nc'
 elif len(args) == 1:
     nc_outfile = args[0]
 else:
@@ -35,6 +35,9 @@ else:
 
 if __name__ == "__main__":
 
+    xdim = 'x'
+    ydim = 'y'
+
     # define output grid, these are the extents of Mathieu's domain (cell
     # corners)
     e0 = -638000
@@ -45,8 +48,8 @@ if __name__ == "__main__":
     # Add a buffer on each side such that we get nice grids up to a grid spacing
     # of 36 km.
 
-    buffer_e = 40650
-    buffer_n = 22000
+    buffer_e = 148650
+    buffer_n = 130000
     e0 -= buffer_e
     n0 -= buffer_n
     e1 += buffer_e
@@ -72,40 +75,95 @@ if __name__ == "__main__":
 
     lon, lat = proj(ee, nn, inverse=True)
 
+    # number of grid corners
+    grid_corners = 4
+    # grid corner dimension name
+    grid_corner_dim_name = "nv4"
+
+    # array holding x-component of grid corners
+    gc_easting = np.zeros((M, grid_corners))
+    # array holding y-component of grid corners
+    gc_northing = np.zeros((N, grid_corners))
+    # array holding the offsets from the cell centers
+    # in x-direction (counter-clockwise)
+    de_vec = np.array([-de / 2, de / 2, de / 2, -de / 2])
+    # array holding the offsets from the cell centers
+    # in y-direction (counter-clockwise)
+    dn_vec = np.array([-dn / 2, -dn / 2, dn / 2, dn / 2])
+    # array holding lat-component of grid corners
+    gc_lat = np.zeros((N, M, grid_corners))
+    # array holding lon-component of grid corners
+    gc_lon = np.zeros((N, M, grid_corners))
+    
+    for corner in range(0, grid_corners):
+        ## grid_corners in x-direction
+        gc_easting[:, corner] = easting + de_vec[corner]
+        # grid corners in y-direction
+        gc_northing[:, corner] = northing + dn_vec[corner]
+        # meshgrid of grid corners in x-y space
+        gc_ee, gc_nn = np.meshgrid(
+            gc_easting[:, corner], gc_northing[:, corner])
+        # project grid corners from x-y to lat-lon space
+        gc_lon[:, :, corner], gc_lat[:, :, corner] = proj(
+            gc_ee, gc_nn, inverse=True)
+
+
     nc = CDF(nc_outfile, 'w', format=fileformat)
 
-    nc.createDimension("x", size=easting.shape[0])
-    nc.createDimension("y", size=northing.shape[0])
-
-    var = 'x'
-    var_out = nc.createVariable(var, 'f', dimensions=("x"))
-    var_out.axis = "X"
+    nc.createDimension(xdim, size=easting.shape[0])
+    nc.createDimension(ydim, size=northing.shape[0])
+    
+    var = xdim
+    var_out = nc.createVariable(var, 'f', dimensions=(xdim))
+    var_out.axis = xdim
     var_out.long_name = "X-coordinate in Cartesian system"
     var_out.standard_name = "projection_x_coordinate"
     var_out.units = "meters"
     var_out[:] = easting
 
-    var = 'y'
-    var_out = nc.createVariable(var, 'f', dimensions=("y"))
-    var_out.axis = "Y"
+    var = ydim
+    var_out = nc.createVariable(var, 'f', dimensions=(ydim))
+    var_out.axis = ydim
     var_out.long_name = "Y-coordinate in Cartesian system"
     var_out.standard_name = "projection_y_coordinate"
     var_out.units = "meters"
     var_out[:] = northing
 
     var = 'lon'
-    var_out = nc.createVariable(var, 'f', dimensions=("y", "x"))
+    var_out = nc.createVariable(var, 'f', dimensions=(ydim, xdim))
     var_out.units = "degrees_east"
     var_out.valid_range = -180., 180.
     var_out.standard_name = "longitude"
+    var_out.bounds = "lon_bnds"
     var_out[:] = lon
 
     var = 'lat'
-    var_out = nc.createVariable(var, 'f', dimensions=("y", "x"))
+    var_out = nc.createVariable(var, 'f', dimensions=(ydim, xdim))
     var_out.units = "degrees_north"
     var_out.valid_range = -90., 90.
     var_out.standard_name = "latitude"
+    var_out.bounds = "lat_bnds"
     var_out[:] = lat
+
+    nc.createDimension(grid_corner_dim_name, size=grid_corners)
+
+    var = 'lon_bnds'
+    # Create variable 'lon_bnds'
+    var_out = nc.createVariable(
+        var, 'f', dimensions=(ydim, xdim, grid_corner_dim_name))
+    # Assign units to variable 'lon_bnds'
+    var_out.units = "degreesE"
+    # Assign values to variable 'lon_nds'
+    var_out[:] = gc_lon
+        
+    var = 'lat_bnds'
+    # Create variable 'lat_bnds'
+    var_out = nc.createVariable(
+        var, 'f', dimensions=(ydim, xdim, grid_corner_dim_name))
+    # Assign units to variable 'lat_bnds'
+    var_out.units = "degreesN"
+    # Assign values to variable 'lat_bnds'
+    var_out[:] = gc_lat
 
     var = 'dummy'
     var_out = nc.createVariable(
@@ -114,7 +172,7 @@ if __name__ == "__main__":
         dimensions=(
             "y",
             "x"),
-        fill_value=-9999)
+        fill_value=-2e9)
     var_out.units = "meters"
     var_out.long_name = "Just A Dummy"
     var_out.comment = "This is just a dummy variable for CDO."
