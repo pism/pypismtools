76f3d6d2a2e66b39cf64e0ab9abb445148dccfbc
Constantine Khroulev <ckhroulev@alaska.edu>
2016-05-11 15:52:41 -0800

    Ignore IndexError when copying scalar and 1D variables.

--- scripts/extract_profiles.py
+++ scripts/extract_profiles.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2015 Constantine Khroulev and Andy Aschwanden
+# Copyright (C) 2015, 2016 Constantine Khroulev and Andy Aschwanden
 #
 
 # nosetests --with-coverage --cover-branches --cover-html
@@ -1295,6 +1295,8 @@ def extract_variable(nc_in, nc_out, profiles, var_name):
         except RuntimeError as e:
             print "extract_profiles failed while writing {}".format(var_name)
             raise
+        except IndexError:
+            print "Failed to copy {}. Ignoring it...".format(var_name)
 
     copy_attributes(var_in, var_out)
     print("  - done with %s" % var_name)
